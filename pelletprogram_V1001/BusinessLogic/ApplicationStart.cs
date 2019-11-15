5dfde9e8064dcc7ce1052e48559b1f19814a3ca4
Aaron Carrick <aaronlevicarrick@outlook.com>
2019-11-14 22:42:02 -0800

    Added class ApplicationStartup with public method checkday

---
+++ pelletprogram_V1001/BusinessLogic/ApplicationStart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    class ApplicationStart
+    {
+        public DayOfWeek Today;
+        public string message;
+        public string CheckDay()
+        {
+            Today = DayOfWeek.Thursday;
+            if(Today == DayOfWeek.Thursday)
+            {
+                message = "You are late!";
+            }
+            return message;
+
+        }
+
+    }
+}
