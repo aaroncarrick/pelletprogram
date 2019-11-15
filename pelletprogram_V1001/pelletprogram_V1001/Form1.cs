27e2da9395eba64b2750264c72634b857fc98b4e
Aaron Carrick <aaronlevicarrick@outlook.com>
2019-11-14 21:56:13 -0800

    Adding PopUp box code under button on form

--- pelletprogram_V1001/pelletprogram_V1001/Form1.cs
+++ pelletprogram_V1001/pelletprogram_V1001/Form1.cs
@@ -16,5 +16,10 @@ namespace pelletprogram_V1001
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hi");
+        }
     }
 }
