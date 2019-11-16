b55ecdc11b3b863414847b142632bc5cc3f07a9b
Aaron Carrick <20379391+aaroncarrick@users.noreply.github.com>
2019-11-15 17:40:39 -0800

    Increased size of button

--- pelletprogram_V1001/pelletprogram_V1001/Form1.Designer.cs
+++ pelletprogram_V1001/pelletprogram_V1001/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(477, 80);
+            this.button1.Location = new System.Drawing.Point(228, 179);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(137, 88);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
