9e47247fd68941b95c14ea41180b97299bc7e93e
Aaron Carrick <20379391+aaroncarrick@users.noreply.github.com>
2019-11-15 18:13:22 -0800

    Added Member class and extracted interface

---
+++ pelletprogram_V1001/BusinessLogic/IMember.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BusinessLogic
+{
+    public interface IMember
+    {
+        string FirstName { get; set; }
+        System.Collections.Generic.List<IMember> History { get; set; }
+        bool IsAceRewardsMember { get; set; }
+        string LastName { get; set; }
+        DateTime MemberAction { get; set; }
+        string Notice { get; set; }
+        string PhoneNumber { get; set; }
+    }
+}
---
+++ pelletprogram_V1001/BusinessLogic/Member.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Member : IMember
+    {
+        public string FirstName { get; set; }
+        public string LastName  { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsAceRewardsMember { get; set; }
+        public DateTime MemberAction { get; set; }
+        public string Notice { get; set; }
+        public List<IMember> History { get; set; }
+    }
+}
